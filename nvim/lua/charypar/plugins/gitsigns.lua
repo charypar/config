9300a616aa8e00046ed508c76a00434199ce3196
Viktor Charypar <charypar@gmail.com>
2024-05-05 21:22:23 +0100

    Configure bindings for git stuff

--- nvim/lua/charypar/plugins/gitsigns.lua
+++ nvim/lua/charypar/plugins/gitsigns.lua
@@ -1,15 +1,28 @@
 return {
     "lewis6991/gitsigns.nvim",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    event = { "BufReadPre", "BufReadPost", "BufWritePost", "BufNewFile" },
     config = function()
-        local gitsigns = require("gitsigns")
+        local gs = require("gitsigns")
 
-        gitsigns.setup({
+        gs.setup({
             current_line_blame = true,
             current_line_blame_opts = {
-                virt_text_pos = 'right_align',
+                virt_text_pos = "right_align",
                 delay = 200,
             },
+            on_attach = function(bufnr)
+                local function map(mode, l, r, desc)
+                    vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+                end
+
+                map("n", "<leader>hb", function()
+                    gs.blame_line({ full = true })
+                end, "Blame current line")
+                map("n", "<leader>hd", gs.diffthis, "Diff this")
+                map("n", "<leader>hD", function()
+                    gs.diffthis("~")
+                end, "Diff this ~")
+            end,
         })
     end,
 }
